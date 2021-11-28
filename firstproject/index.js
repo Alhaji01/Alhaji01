1198e25797101fa6cc9f3d367a35d7f0d624a643
Alhaji Alpha Fofana <alhajialphafofana@Alhajis-MacBook-Air.local>
2021-11-28 16:12:28 +0200

    first commit

---
+++ firstproject/index.js
@@ -0,0 +1,43 @@
+const enterButton = document.getElementById ("enter")
+const input = document.getElementById ("userInput")
+const ul = document.querySelector ("ul")
+const item = document.getElementsByTagName ("li")
+
+function hasimput ( ) {
+return input.value.length > 0
+}
+
+function createListElement() {
+    const li = document.createElement ("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = " ";
+}
+
+   
+function crossOut() {
+    li.createList.toggle("dine")
+}
+
+li.addeventListener("lick",crossOut);
+const dBtn = createListElement("button");
+dBtn.appendChild(document.createTextNode("X"));
+
+function deleteListitem() {
+    li.classList.add("delete")
+   }
+
+
+
+
+function   addListAfterKeypress(event) {
+    if (hasimput() && event.which ===13) {
+        createListElement();
+    }
+    }
+ function addListAfterClick() {
+    createListElement();
+}
+
+enterButton.addEventListener("click",addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
---
+++ project#1/js/index.js
@@ -0,0 +1,6 @@
+const minimumAge = 40
+const maximumAge = 60
+const totalAge = minimumAge + maximumAge
+
+
+console.log (totalAge)
